848a5397af3ebdf50bd9eb4a92c78d11526576ba
babyplutokurt <vertigorockkurt@163.com>
2023-04-20 02:39:37 -0400

    4/20 backup

---
+++ contracts/My_721.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Test_721 is ERC721, ERC721Burnable, Ownable {
+    constructor() ERC721("Test_721", "T721") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
